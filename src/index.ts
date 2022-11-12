13984907e3aca41c0d55d6beaf8f778ed8d7cb7e
Felix T <79270989+tikkanenfelix@users.noreply.github.com>
2022-11-12 13:57:28 +0100

    added a transfer sol function
    from the initialised account to a random one.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import * as Web3 from '@solana/web3.js';
 import * as fs from 'fs';
 import dotenv from 'dotenv';
 
+const PROGRAM_ID = new Web3.PublicKey("ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa")
+const PROGRAM_DATA_PUBLIC_KEY = new Web3.PublicKey("Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod")
+
 dotenv.config();
 
 async function airdropSolIfNeeded(
@@ -54,6 +57,53 @@ async function initializeKeypair(
 
     return keypairFromSecret;
   }
+
+  async function pingProgram(connection: Web3.Connection, payer: Web3.Keypair) {
+    const transaction = new Web3.Transaction()
+    const instruction = new Web3.TransactionInstruction({
+      // Instructions need 3 things 
+      
+      // 1. The public keys of all the accounts the instruction will read/write
+      keys: [
+        {
+          pubkey: PROGRAM_DATA_PUBLIC_KEY,
+          isSigner: false,
+          isWritable: true
+        }
+      ],
+      
+      // 2. The ID of the program this instruction will be sent to
+      programId: PROGRAM_ID
+      
+      // 3. Data - in this case, there's none!
+    })
+  
+    transaction.add(instruction)
+    const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transaction, [payer])
+  
+    console.log(
+      `Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+      )
+  }
+  
+  async function transferSol(connection:Web3.Connection, payer: Web3.Keypair){
+    const toKeypair = Web3.Keypair.generate();
+
+    const lamportsToSend = 100_000_000;
+
+    const transferTransaction = new Web3.Transaction().add(
+        Web3.SystemProgram.transfer({
+            fromPubkey: payer.publicKey,
+            toPubkey: toKeypair.publicKey,
+            lamports: lamportsToSend,
+
+        })
+    );
+
+    console.log(`from ${payer.publicKey.toBase58()}, to ${toKeypair.publicKey}, this much: ${lamportsToSend / Web3.LAMPORTS_PER_SOL}`)
+    const transactionSignature = await Web3.sendAndConfirmTransaction(connection, transferTransaction, [payer])
+    console.log(`Transaction https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`)
+  }
   
 
 
@@ -62,6 +112,8 @@ async function main() {
     const signer = await initializeKeypair(connection);
   
     console.log("Public key:", signer.publicKey.toBase58());
+    await pingProgram(connection, signer)
+    await transferSol(connection, signer)
   }
   
 

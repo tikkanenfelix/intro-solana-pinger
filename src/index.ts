fb99e571f2145dacde90b09ffbaf2b8f09b901f1
Felix T <79270989+tikkanenfelix@users.noreply.github.com>
2022-11-12 08:29:30 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
-async function main() {}
+import * as Web3 from '@solana/web3.js';
+import * as fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+async function main() {
+}
 
 main()
-    .then(() => {
-        console.log("Finished successfully.")
-        process.exit(0)
-    })
-    .catch((error) => {
-        console.log(error)
-        process.exit(1)
-    })
+  .then(() => {
+    console.log('Finished successfully');
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
